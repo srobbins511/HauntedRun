2abbf1bfe2452a4f8dd37756d8a021e83535a170
Sean Robbins <srobbins@chapman.edu>
2020-04-08 09:59:19 -0700

    Started Game Manager and Level Manager

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,23 +4,45 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    #region Variables
 
+    /// <summary>
+    /// The two variables used for determining movement directions
+    /// movingUp triggers an upward movement when positive and a downward movement when negative
+    /// movingLeft triggers horizontal movement based on when it is positive or negative
+    /// </summary>
     public float movingUp;
     public float movingLeft;
+
+    //a reference to the current position and transform of the player
     public Transform currentPos;
+
+    /// <summary>
+    /// Variables that show if a player is moving and
+    /// if thier movement is being affected by a sprint command
+    /// </summary>
     public bool moved;
     public bool isSprinting;
 
+    //reference to the character controller attached to the player object
     private CharacterController c;
 
     [SerializeField]
+    [Tooltip("Determines how fast that the player will be moving, should be positive")]
     private float MovementSpeed;
 
     [SerializeField]
     [Tooltip("Increases movement speed by percentage, should be  value between 0 and 1")]
     private float SprintSpeed;
 
-    // Start is called before the first frame update
+    #endregion
+
+
+    /// <summary>
+    /// initializes the characeter controller reference and stores the current position at the
+    /// start
+    /// Start is called before the first frame update
+    /// </summary>
     void Start()
     {
         c = gameObject.GetComponent<CharacterController>();
@@ -36,11 +58,17 @@ public class CharacterMovement : MonoBehaviour
         
     }
 
+    /// <summary>
+    /// Checks the inputs registered everyframe to determine if the player should move
+    /// returns a bool value as to whether the character needs to move
+    /// </summary>
+    /// <returns></returns>
     private bool checkInputs()
     {
-        Debug.Log("CheckInputs Called");
         movingUp = 0f;
         movingLeft = 0f;
+
+
         if(Input.GetKey(KeyCode.W))
         {
             movingUp = MovementSpeed;
@@ -71,9 +99,12 @@ public class CharacterMovement : MonoBehaviour
         return (movingUp != 0 || movingLeft != 0);
     }
 
+    /// <summary>
+    /// Method to move the player by 
+    /// leveraging the character controller move method
+    /// </summary>
     private void Move()
     {
-        Debug.Log("MoveCalled");
         if (isSprinting)
             c.Move(new Vector3(movingLeft + (movingLeft * SprintSpeed), movingUp + (movingUp * SprintSpeed), 0) * Time.deltaTime);
         else
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public bool Key1;
+    public bool Key2;
+    public bool Key3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnComplete()
+    {
+
+    }
+}
