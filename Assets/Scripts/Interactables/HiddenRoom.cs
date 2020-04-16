88c6d15d4120a72819f4473ac2eb9fee80f82977
Sean Robbins <srobbins@chapman.edu>
2020-04-16 13:22:16 -0700

    Hidden Rooms Script

---
+++ Assets/Scripts/Interactables/HiddenRoom.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenRoom : Interactable
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    private GameObject Entrance;
+
+    [SerializeField]
+    private GameObject Room;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Interact()
+    {
+        Entrance.SetActive(false);
+        Room.SetActive(true);
+    }
+}
