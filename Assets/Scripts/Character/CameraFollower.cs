7af7262b2003ce74ec350dc25fa317543e8a0316
Sean Robbins <srobbins@chapman.edu>
2020-04-17 13:52:30 -0700

    Camera Resizing with Mouse scroll button

--- Assets/Scripts/Character/CameraFollower.cs
+++ Assets/Scripts/Character/CameraFollower.cs
@@ -5,17 +5,39 @@ using UnityEngine;
 public class CameraFollower : MonoBehaviour
 {
     public Transform playerLocation;
+    public int MinSize;
+    public int MaxSize;
+
+    private float size;
 
 
     public void Start()
     {
         playerLocation = GameManager.Instance.Player.transform;
+        gameObject.GetComponent<Camera>().orthographic = true;
+        size = gameObject.GetComponent<Camera>().orthographicSize;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         gameObject.transform.position = new Vector3(Mathf.Lerp(gameObject.transform.position.x, playerLocation.position.x, .1f), Mathf.Lerp(gameObject.transform.position.y, playerLocation.position.y, .1f), gameObject.transform.position.z);
+        ReSizeCamera();
+    }
+
+    private void ReSizeCamera()
+    {
+        if (size < MaxSize && size > MinSize)
+        {
+            size = gameObject.GetComponent<Camera>().orthographicSize -= 10 * Input.GetAxis("Mouse ScrollWheel");
+        }
+        else if (size > MaxSize && Input.GetAxis("Mouse ScrollWheel")>0)
+        {
+            size = gameObject.GetComponent<Camera>().orthographicSize -= 10 * Input.GetAxis("Mouse ScrollWheel");
+        }
+        else if(size < MinSize && Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            size = gameObject.GetComponent<Camera>().orthographicSize -= 10 * Input.GetAxis("Mouse ScrollWheel");
+        }
     }
 }
