7d3cc918f752ed5cb02784bd1d0e05ae19ece7c5
Sean Robbins <srobbins@chapman.edu>
2020-04-24 13:55:05 -0700

    Updates

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public void Quit()
+    {
+        GameManager.Instance.Quit();
+    }
+
+    public void Startgame()
+    {
+        GameManager.Instance.Startgame();
+    }
+}
