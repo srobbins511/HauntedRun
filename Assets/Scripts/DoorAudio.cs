21931f129dd6481d1f94943ca60095469ba2d970
kayleew702 <60621712+kayleew702@users.noreply.github.com>
2020-05-11 11:17:51 -0700

    door sound

---
+++ Assets/Scripts/DoorAudio.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAudio : MonoBehaviour
+{
+    AudioSource audioSource;
+    AudioClip audioClip;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play(0);
+        Debug.Log("started");
+    }
+}
