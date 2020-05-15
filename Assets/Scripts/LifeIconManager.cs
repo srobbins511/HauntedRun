618a0afa3a49962e9d30bc2f788ffbc8225a4674
Sean Robbins <srobbins@chapman.edu>
2020-05-15 16:49:13 -0700

    Player Life Icons

--- Assets/Scripts/LifeIconManager.cs
+++ Assets/Scripts/LifeIconManager.cs
@@ -16,29 +16,41 @@ public class LifeIconManager : MonoBehaviour
 
     public int prevLifeCount;
 
+    public bool run;
+
     public void Start()
     {
+        run = false;
         Invoke("Run", 1f);
     }
 
     public void Update()
     {
-
-        if (GameManager.Instance.numLives < prevLifeCount)
+        if (run)
         {
-            OnDeath();
+            if (GameManager.Instance.numLives < prevLifeCount)
+            {
+                OnDeath();
+            }
+            if (GameManager.Instance.numLives > prevLifeCount)
+            {
+                OnLifeGain();
+            }
         }
 
     }
     public void Run()
     {
+        run = true;
         P = GameObject.FindGameObjectWithTag("Player");
         Icons = new List<GameObject>();
         NumLives = GameManager.Instance.numLives;
+        prevLifeCount = NumLives;
+
         for (int i = 0; i < NumLives; ++i)
         {
             temp = Canvas.Instantiate(PlayerIconPrefab, gameObject.transform.position, gameObject.transform.rotation, gameObject.transform);
-            temp.transform.localPosition += new Vector3(i * 50, 0);
+            temp.transform.localPosition += new Vector3(-i * 50, 0);
             Icons.Add(temp);
         }
 
@@ -47,7 +59,24 @@ public class LifeIconManager : MonoBehaviour
 
     public void OnDeath()
     {
-        Icons[prevLifeCount].GetComponent<Image>().color = new Color(Icons[prevLifeCount].GetComponent<Image>().color.r, Icons[prevLifeCount].GetComponent<Image>().color.g, Icons[prevLifeCount].GetComponent<Image>().color.b, .3f);
-        prevLifeCount++;
+        Icons[--prevLifeCount].GetComponent<Image>().color = new Color(Icons[prevLifeCount].GetComponent<Image>().color.r, Icons[prevLifeCount].GetComponent<Image>().color.g, Icons[prevLifeCount].GetComponent<Image>().color.b, .3f);
+    }
+
+    public void OnLifeGain()
+    {
+        Debug.Log("Run Called");
+        if (NumLives == prevLifeCount)
+        {
+            temp = Canvas.Instantiate(PlayerIconPrefab, gameObject.transform.position, gameObject.transform.rotation, gameObject.transform);
+            temp.transform.localPosition += new Vector3(Icons[prevLifeCount-1].transform.localPosition.x - 50, 0);
+            Icons.Add(temp);
+            NumLives++;
+            prevLifeCount++;
+        }
+        else
+        {
+            Icons[prevLifeCount].GetComponent<Image>().color = new Color(Icons[prevLifeCount].GetComponent<Image>().color.r, Icons[prevLifeCount].GetComponent<Image>().color.g, Icons[prevLifeCount].GetComponent<Image>().color.b, 1f);
+            prevLifeCount++;
+        }
     }
 }
